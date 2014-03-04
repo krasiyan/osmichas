2b70536a81fbc25b965075bc80ad7208369a677a
Krasiyan <krasiyan@devlabs.bg>
2014-03-04 04:28:58 +0200

    added google tracking visual fixes added sample data added navicat tunnel_mysql.php

--- osmichas/application/views/frontend/footer.php
+++ osmichas/application/views/frontend/footer.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct access allowed!'); ?>
-<p class="text-muted">
+<p class="text-muted text-center">
 	Проект изготвен за XII Национална олимпиада по Информационни технологии
-<span class="pull-right">автор: Красиян Красимиров Неделчев XII кл., МГ "Д-р. Петър Берон" гр. Варна</span>
+	<span class="pull-right">автор: Красиян Красимиров Неделчев XII кл., МГ "Д-р. Петър Берон" гр. Варна</span>
 </p>
